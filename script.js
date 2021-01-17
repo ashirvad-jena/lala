555d18435ded1b2e0f499b6bbc15fcad1a0c1e09
Ashirvad Jena <ashirvad.aj@yahoo.com>
2021-01-17 21:22:20 +0530

    updated balance in ui

--- script.js
+++ script.js
@@ -13,21 +13,21 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Amar',
+  owner: 'Honey Singh',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Akbar',
+  owner: 'Harpreet Kaur',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Anthony',
+  owner: 'Johny Simpson',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -83,9 +83,37 @@ const displaymovements = movements => {
   });
 };
 
+const calcPrintBalance = movements => {
+  const balance = movements.reduce(
+    (accumulator, current) => accumulator + current,
+    0
+  );
+  labelBalance.textContent = `₹${balance}`;
+};
+
 displaymovements(account1.movements);
+calcPrintBalance(account1.movements);
+
+/*
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const rupeeToUsd = 0.014;
+const movementsUsd = movements.map(movement => movement * rupeeToUsd);
+console.log(movementsUsd);
+
+const createUsernames = accountsE => {
+  let acc = [...accountsE];
+  acc.forEach(user => {
+    user.username = user.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+console.log(createUsernames(accounts));
+console.log(accounts);
 
 for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
@@ -116,3 +144,4 @@ console.log(arr2);
 
 console.log(arr.concat(arr2));
 console.log([...arr, ...arr2]);
+*/
